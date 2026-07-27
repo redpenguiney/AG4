5be96e9c5f4e2fb6f76f21a334a298e317c8824f
redpenguiney <119638232+redpenguiney@users.noreply.github.com>
2026-07-27 16:13:39 -0700

    turns out gldrawbuffer was illegal there???

--- Engine/framebuffer.cpp
+++ Engine/framebuffer.cpp
@@ -153,5 +153,5 @@ void Framebuffer::Unbind() {
     glViewport(0, 0, windowWidth, windowHeight);
     
     glBindFramebuffer(GL_FRAMEBUFFER, 0); // TODO: other binding locations
-    glDrawBuffer(GL_FRONT_AND_BACK);
+    //glDrawBuffer(GL_BACK);
 }
